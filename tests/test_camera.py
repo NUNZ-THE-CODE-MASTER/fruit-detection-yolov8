b9d3362d99731fb98709cf0296c4ccc4f9fdcacb
Aba Nuni <102610560+NUNZ-THE-CODE-MASTER@users.noreply.github.com>
2025-12-07 21:30:38 +0400

    Add camera test

---
+++ tests/test_camera.py
@@ -0,0 +1,17 @@
+"""Test camera"""
+import cv2
+
+cap = cv2.VideoCapture(0)
+print("Press 'q' to quit")
+
+while True:
+    ret, frame = cap.read()
+    if ret:
+        cv2.imshow('Camera', frame)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+print("Camera works!")
